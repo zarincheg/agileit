db5012a359f3af5483d8e17cea703a36e5a9ca41
Zorin Kirill <zarincheg@gmail.com>
2013-06-07 14:19:22 +0300

    Бутстрап добавлен как подмодуль. Скрипты интерфейса перенесены в статику.

--- rest.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo json_encode(['author' => 'Ivan Buvan',
-				  'date' => '25/05/2013',
-				  'text' => 'Pages fetched with POST are never cached, so the cache and ifModified options in jQuery.ajaxSetup() have no effect on these requests.'
-				 ]);
